b956590cb081a768c3a55c0343786ee415c2bf71
MikhailRastopchin <mrastopchin89@gmail.com>
2022-02-10 12:44:51 +0300

    [page] Map: set markers, polylines, zoombuttons.

--- lib/pages/map.dart
+++ lib/pages/map.dart
@@ -1,8 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map/plugin_api.dart';
+import 'package:la_navigation/models/another_fox_track.dart';
 import 'package:la_navigation/services/coordinates.dart';
-import 'package:latlong2/latlong.dart';
 import 'package:provider/provider.dart';
+
+import '../widgets/zoombuttons.dart';
+
+
+const _kMarkerColors = [
+  Colors.red,
+  Colors.blue,
+  Colors.green,
+  Colors.yellowAccent,
+  Colors.purple,
+  Colors.orange,
+  Colors.brown,
+  Colors.indigo,
+  Colors.lightBlue,
+  Colors.teal
+];
 
 
 class MapPage extends StatefulWidget
@@ -20,34 +37,74 @@ class _MapPageState extends State<MapPage>
   Widget build(BuildContext context)
   {
     final coordinates = context.watch<CoordinatesService>();
+    final markers = <Marker>[];
+    final polilines = <Polyline>[];
+    for (var index = 0; index < coordinates.tracks.length; index++) {
+      final dividedIndex = index > 9 ? index % 10 : index;
+      assert(dividedIndex < _kMarkerColors.length);
+      final color = _kMarkerColors[dividedIndex];
+      final track = coordinates.tracks[index];
+      markers.add(_buildMarker(track, color));
+      polilines.add(_buildPoliline(track, color));
+    }
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(24.0),
-        child: FlutterMap(
-          options: MapOptions(
-            center: coordinates.ownCoordinate,
-            zoom: 13.0,
-          ),
-          layers: [
-            TileLayerOptions(
-              urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-              subdomains: ['a', 'b', 'c'],
-              tileProvider: const FileTileProvider()
-            ),
-            MarkerLayerOptions(
-              markers: [
-                Marker(
-                  width: 80.0,
-                  height: 80.0,
-                  point: LatLng(56.99, 40.57),
-                  builder: (ctx) =>
-                  const Icon(Icons.location_on, color: Colors.red, size: 40.0),
-                ),
-              ],
-            ),
+      appBar: AppBar(title: const Text('Карта'), centerTitle: true),
+      body: FlutterMap(
+        options: MapOptions(
+          center: coordinates.ownCoordinate,
+          zoom: 18.0,
+          plugins: [
+            ZoomButtonsPlugin(),
           ],
         ),
+        layers: [
+          TileLayerOptions(
+            urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+            subdomains: ['a', 'b', 'c'],
+          ),
+          MarkerLayerOptions(markers: markers),
+          PolylineLayerOptions(polylines: polilines),
+        ],
+        nonRotatedLayers: [
+          ZoomButtonsPluginOption(
+            minZoom: 2,
+            maxZoom: 18,
+            mini: true,
+            padding: 10,
+            alignment: Alignment.bottomRight,
+          ),
+        ],
       ),
     );
+  }
+
+  Marker _buildMarker(final AnotherFoxTrack track, final Color color)
+  {
+    final theme = Theme.of(context);
+    return Marker(
+      key: ValueKey(track.currentCoordinate),
+      width: 120,
+      height: 30.0,
+      point: track.currentCoordinate,
+      anchorPos: AnchorPos.exactly(Anchor(105.0, 0.0)),
+      builder: (context) => Row(
+        children: [
+          Icon(Icons.location_on, color: color, size: 30.0),
+          const SizedBox(width: 3.0),
+          SizedBox(
+            width: 77.0,
+            child: Text(track.name, style: theme.textTheme.bodyText2!.copyWith(
+              color: color
+            )),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Polyline _buildPoliline(final AnotherFoxTrack track, final Color color)
+  {
+    final points = track.track.values.toList();
+    return Polyline(points: points, strokeWidth: 4.0, color: color);
   }
 }
